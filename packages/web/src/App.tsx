ae10c5e2cc3d8bbd68717adcfbaeb93e2cf7e2e2
spetushkov <sergey.petushkov@gmail.com>
2022-03-28 15:41:35 +0200

    feat: monorepo

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,11 +1,11 @@
-import { Button } from '@ibm/common2';
+import { CustomButton } from '@ibm/common2';
 import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Button textColor='red'>test button</Button>
+      <CustomButton textColor='red'>test button</CustomButton>
     </div>
   );
 }
